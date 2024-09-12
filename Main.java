735317f7220ffd4013bccbe53d4c0437da92eb61
Jos143 <joseguadalupeperezcortez2006@gamil.com>
2024-09-11 23:05:16 -0600

    Tarea 3

---
+++ Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Seccón 1, Mostrar información
+        System.out.println("Sección 1: información de nuestro libro favorito");
+        System.out.println("El título: Todos mis futuros son contigo");
+        System.out.println("Autor: Marwan");
+        System.out.println("Año de publicacion: 2015");
+        System.out.println("Género: Poesía");
+        System.out.println("Número de páginas: 250");
+
+        //sección 2; declaracion de variables
+        String nPais = "Alemania";
+        String capital = "Berlín";
+        String idiomaF = "Alemán";
+        int Poblacion = 85;
+        String moneda = "Euro";
+
+        //Mostrar variables
+        System.out.println("Sección 2: información un país que deseo visitar");
+        System.out.println("País:" + nPais);
+        System.out.println("Capita:" + capital);
+        System.out.println("Idioma oficial: " + idiomaF);
+        System.out.println("Su población es de " + Poblacion + " millones");
+        System.out.println("Su moneda oficial es:" + moneda);
+
+        
+        //Sección 3, declaracion de variables
+        String noPersonaje = "Benito Juarez";
+        String fechaNacimiento = "21 de marzo de 1806";
+        String nacionalidad = "Mexicana";
+        String ocupacion = "Político, Abogado, Juez";
+        String logrosD = "Leyes de reforma";
+
+        // Mostrar variables
+        System.out.println("Sección 3: información de un parsonaje historico favorito");
+        System.out.println("Nombre: " + noPersonaje);
+        System.out.println("Fecha de nacimiento " + fechaNacimiento);
+        System.out.println("Nacionalidad " + nacionalidad);
+        System.out.println("Ocupacion " + ocupacion);
+        System.out.println("Logros " + logrosD);
+
+
+
+    }
+}
