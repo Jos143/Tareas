050f00eec76fb29220bf36c9ab78ec90872f0f8d
Jos143 <joseguadalupeperezcortez2006@gmail.com>
2024-09-13 00:22:40 -0600

    Add files via upload

---
+++ tarea 4.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//PRIMER PARTE
+        double operacion1 = ((12+8)*(5-3));
+        double operacion2 = (6/2);
+        double operacion3 = ((3*4)+(8/2));
+        double resultadonOperacion1 = (operacion1-operacion2)/operacion3;
+        System.out.println( resultadonOperacion1);
+
+//SEGUNDA PARTE
+
+      double re = (((9*5)-(15+3)+(8*2)-(12/4))*(7+2)-((6*3)/2)) / (2+  ((3*2)/(4-2))   ) +(5* ((8/4)+1))-6/(2+1);
+      double re1 =((  ((4+2)*(7-1))/(3+1)   )+  ((3*2)-(5+1))*(8/2))/(3+( (6/2)+(7/2)     )) + (2*(3+(8/4)));
+      double Resultadofinal2= re/re1;
+
+        System.out.println(Resultadofinal2);
+
+//PARTE TRES
+
+        double procesoResultado  = ((6+4)*3)-(18/2);
+        double procesoResultado1 =(8-5)+(3*2);
+        double procesoResultado2 =procesoResultado/procesoResultado1;
+        System.out.println(procesoResultado2);
+    }
+}
