82b324aefcabfd0bbff076fc6829f68156a3a81c
Jos143 <joseguadalupeperezcortez2006@gmail.com>
2024-10-15 22:11:30 -0600

    tarea 3/ Tercera unidad

---
+++ Tarea3U3.java
@@ -0,0 +1,126 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        if(3<5&&7>=2){
+            System.out.println("El resultaddo de la operacion 3<5&&7>=2 es verdadero");
+        }else{
+            System.out.println("El resultaddo de la operacion 3<5&&7>=2 es falso");
+        }
+
+        if(10!=8||15==15){
+            System.out.println("El resultado de la operacion 10!=8||15==15 es verdadero");
+        }else{
+            System.out.println("El resultaddo de la operacion 10!=8||15==15 es falso");
+        }
+
+        if(!(5>6)&&2<=3){
+            System.out.println("El resultado de la operacion !(5>6)&&2<=3 es verdadero");
+        }else{
+            System.out.println("El resultaddo de la operacion !(5>6)&&2<=3 es falso");
+        }
+
+        if((8==8||3<1)&&6>2){
+            System.out.println("El resultado de la operacion (8==8||3<1)&&6>2 es verdeadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion (8==8||3<1)&&6>2 es falso");
+        }
+
+        if(12!=10&&(4<8||9==9)){
+            System.out.println("El resultado de la operacion 12!=10&&(4<8||9==9) es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion 12!=10&&(4<8||9==9) es falso");
+        }
+
+        if((3+5)==8||(7*2)<=14){
+            System.out.println("El resultado de la operacion (3+5)==8||(7*2)<=14  es verdadero");
+        }else{
+            System.out.println("El resultaddo de la operacion (3+5)==8||(7*2)<=14 es falso");
+        }
+
+        if(!(9<=5)&&(4==4||6!=6)){
+            System.out.println("El resultado de la operacion !(9<=5)&&(4==4||6!=6) es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion !(9<=5)&&(4==4||6!=6) es falso");
+        }
+
+        if(10%2==0&&(5>1||12!=10)){
+            System.out.println("El resultado de la operacion 10%2==0&&(5>1||12!=10) es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion 10%2==0&&(5>1||12!=10) es falso");
+        }
+
+        if ((3*3==9)&&!(8<5||7>=9)){
+            System.out.println("El resultado de la operacion (3*3==9)&&!(8<5||7>=9 es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion (3*3==9)&&!(8<5||7>=9) es falso");
+        }
+
+        if (!(5+5!=10)&&(2*3>5||6==7)){
+            System.out.println("El resultado de la operacion !(5+5!=10)&&(2*3>5||6==7) es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion !(5+5!=10)&&(2*3>5||6==7) es falso");
+        }
+
+        if(20/4==5||!(10%3==1&&4>=3)){
+            System.out.println("El resultado de la operacion 20/4==5||!(10%3==1&&4>=3) es verdadero");
+        }else{
+            System.out.println("El resultaddo de la operacion 20/4==5||!(10%3==1&&4>=3) es falso");
+        }
+
+        if (!(7>3&&2==2)||(5*2<12&&8>=8)){
+            System.out.println("El resultado de la operacion !(7>3&&2==2)||(5*2<12&&8>=8) es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion !(7>3&&2==2)||(5*2<12&&8>=8) es falso");
+        }
+
+        if ((15-5>8)&&!(3==3||7<1)&&12>=10){
+            System.out.println("El resultado de la operacion (15-5>8)&&!(3==3||7<1)&&12>=10 es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion (15-5>8)&&!(3==3||7<1)&&12>=10 es falso");
+        }
+
+        if (9%3==0&&(8<=12||5+5!=10)&&!(7>8)) {
+            System.out.println("El resultado de la operacion (9%3==0&&(8<=12||5+5!=10)&&!(7>8) es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion (9%3==0&&(8<=12||5+5!=10)&&!(7>8) es falso");
+        }
+
+        if (!(10!=10)&&(5*3==15||2<1)&&9>=9){
+            System.out.println("El resultado de la operacion !(10!=10)&&(5*3==15||2<1)&&9>=9) es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion !(10!=10)&&(5*3==15||2<1)&&9>=9) es falso");
+        }
+
+        if ((4+6==10||2==3)&&!(8<8&&3>2)||7%2==1){
+            System.out.println("El resultado de la operacion (4+6==10||2==3)&&!(8<8&&3>2)||7%2==1 es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion (4+6==10||2==3)&&!(8<8&&3>2)||7%2==1 es falso");
+        }
+
+        if((12/3>4)&&(5==5||10!=10)&&!(8>=9)){
+            System.out.println("El resultado de la operacion (12/3>4)&&(5==5||10!=10)&&!(8>=9) es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion (12/3>4)&&(5==5||10!=10)&&!(8>=9) es falso ");
+        }
+
+        if (!(7+3<10)&&(9/3==3||6<2)&&(5*4>=20)){
+            System.out.println("El resultado de la operacion !(7+3<10)&&(9/3==3||6<2)&&(5*4>=20)) es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion !(7+3<10)&&(9/3==3||6<2)&&(5*4>=20)) es falso");
+        }
+
+        if (((10-2)!=8)&&!(6>=7||2==2)&&(12%3==0)){
+            System.out.println("El resultado de la operacion !(7+3<10)&&(9/3==3||6<2)&&(5*4>=20)) es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion !(7+3<10)&&(9/3==3||6<2)&&(5*4>=20)) es falso");
+        }
+
+        if (!(15>5&&3+2==5)||(9<10&&!(6<=4))&&(7*2!=14)){
+            System.out.println("El resultado de la operacion !(15>5&&3+2==5)||(9<10&&!(6<=4))&&(7*2!=14) es verdadero ");
+        }else{
+            System.out.println("El resultaddo de la operacion !(15>5&&3+2==5)||(9<10&&!(6<=4))&&(7*2!=14) es falso");
+        }
+    }
+}
